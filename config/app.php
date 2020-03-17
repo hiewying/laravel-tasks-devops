a44ee061dc111bab811fbad9e34eaef53d3db22e
hiewying <hiew.san@student.tafesa.edu.au>
2020-03-17 19:02:27 +0000

    edited localhost to 127.0.0.1:8000

--- config/app.php
+++ config/app.php
@@ -51,7 +51,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://127.0.0.1:8000'),
 
     /*
     |--------------------------------------------------------------------------
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,7 +8,7 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public $baseUrl = 'http://localhost';
+    public $baseUrl = 'http://127.0.0.1:8000';
 
     protected function signIn($user = null)
     {
